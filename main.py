8dbd0ac304ac575390059d459a4f6dcd4e7c868b
jithin <97422120+jithin2059@users.noreply.github.com>
2024-01-20 17:06:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,6 +45,6 @@ def upload_file():
                 return render_template("upload.html", error=str(e))
 
     return render_template("upload.html")
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8080, debug=True)
+
+app.run(host="0.0.0.0",port=80, debug=True)
 
